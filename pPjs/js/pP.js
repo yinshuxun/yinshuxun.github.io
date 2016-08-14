b08ac0643a6b0e8b8794ab300bc698f9b48de1b9
shuxun.yin <shuxun.yin@gmail.com>
2016-08-14 20:41:11 +0800

    repaire style bug

--- pPjs/js/pP.js
+++ pPjs/js/pP.js
@@ -1,6 +1,6 @@
 /**
  * @author shuxun.yin
- * picture preview 
+ * picture preview
  */
 +function ($, global) {
     var Pp = function (opts) {
@@ -196,7 +196,7 @@
                 newW = winW * 0.2;
                 newH = newW / width * height;
             }
-            $(".picZoom .content").css({width: newW + 20, height: newH + 95});
+            $(".picZoom .content").css({width: newW, height: newH + 95});
             $(".picZoom img").css({width: newW, height: newH});
         },
         next: function () {
